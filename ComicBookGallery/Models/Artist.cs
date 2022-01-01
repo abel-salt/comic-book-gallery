6906799fbd72492cd2ea69a87bc1b15dafa4d1f2
Abel Sintaro <abel.sintaro@appliedtechnology.se>
2022-01-01 15:42:42 +0100

    Update from  static data to Models

---
+++ ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+         public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Role { get; set; }
+    }
+}
