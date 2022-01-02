55709feb3c4f2e5fd8fafe5867173d2bbb532499
Abel Sintaro <abel.sintaro@appliedtechnology.se>
2022-01-02 11:22:49 +0100

    Add ComicBook data repository

--- ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/Controllers/ComicBookController.cs
@@ -4,29 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Data;
 
 namespace ComicBookGallery.Controllers
 {
     public class ComicBookController : Controller
     {
         // GET: ComicBook
-        public ActionResult Welcome()
-        {
-            ComicBook book = new ComicBook() 
-            {
-                Title = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Role = "Script", Name = "Dan Slott"},
-                    new Artist() { Role = "Pencils", Name = "Humberto Ramos"},
-                    new Artist() { Role = "Inks", Name = "Victor Olazaba"},
-                    new Artist() { Role = "Colors", Name = "Edgar Delgado"},
-                    new Artist() { Role = "Letters", Name = "Chris Eliopoulos"}
-                }
+        private ComicBookRepository _commicBookRepository = null;
 
-            };
+        public ComicBookController()
+        {
+            _commicBookRepository = new ComicBookRepository();
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null) return HttpNotFound();
+
+            ComicBook book = _commicBookRepository.GetComicBook((int)id);
             
             return View(book);
         }
